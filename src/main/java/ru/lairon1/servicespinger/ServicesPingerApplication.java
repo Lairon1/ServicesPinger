9e253e66a78d8018d057f0b481d83cc54dec281e
Admin <evseev_vadyusha03@mail.ru>
2025-08-17 11:40:08 +0300

    init

---
+++ src/main/java/ru/lairon1/servicespinger/ServicesPingerApplication.java
@@ -0,0 +1,13 @@
+package ru.lairon1.servicespinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesPingerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesPingerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lairon1/servicespinger/ServicesPingerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lairon1.servicespinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesPingerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
