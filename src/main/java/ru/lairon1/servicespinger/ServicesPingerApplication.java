f9ec72c3b602730a84f7823789df7435ce20fbe8
Admin <evseev_vadyusha03@mail.ru>
2025-08-17 12:37:04 +0300

    Added application-context.xml

--- src/main/java/ru/lairon1/servicespinger/ServicesPingerApplication.java
+++ src/main/java/ru/lairon1/servicespinger/ServicesPingerApplication.java
@@ -2,7 +2,9 @@ package ru.lairon1.servicespinger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource("classpath:application-context.xml")
 @SpringBootApplication
 public class ServicesPingerApplication {
 
